0bf9fe57bbb9c64f07cf5838e8f68f3606aabd24
distractedDave <dave+git@waterford.org>
2014-11-26 00:22:24 -0700

    svn export

--- aminatorplugins/provisioner/chef.py
+++ aminatorplugins/provisioner/chef.py
@@ -149,9 +149,9 @@ def fetch_chef_payload(payload_url):
     client = pysvn.Client()
     chef_path = "/tmp/chef-repo"
     retval = os.getcwd()
-    print "Directory changed successfully %s" % retval
-    client.export(payload_url, chef_path)
+    print "starting the svn export process %s" % retval
     os.chdir(chef_path)
+    client.export(payload_url, chef_path)
     retval = os.getcwd()
     print "Directory changed successfully %s" % retval
     return 'knife upload . -V'
